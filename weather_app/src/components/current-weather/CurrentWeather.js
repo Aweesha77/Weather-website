1c5c57bd574e0b490d0d260832eb517067c4bb76
Kashuni Aweesha Kodithuwakku <126492783+Aweesha77@users.noreply.github.com>
2024-03-09 20:11:22 +0530

    add css

--- weather_app/src/components/current-weather/CurrentWeather.js
+++ weather_app/src/components/current-weather/CurrentWeather.js
@@ -1,7 +1,21 @@
 import './CurrentWeather.css'
 
 const CurrentWeather = () => {
-    return ('hello');
+    return (
+        <div className="weather">
+          <div className="top">
+            <div>
+              <p className="city">{data.city}</p>
+              <p className="weather-description">{data.weather[0].description}</p>
+            </div>
+            <img
+              alt="weather"
+              className="weather-icon"
+              src={`icons/${data.weather[0].icon}.png`}
+            />
+          </div>
+        </div>
+    );
 }
 
 export default CurrentWeather;
