26e71448ce26f6d3c1cf15a10bf9cc40dae16503
Kashuni Aweesha Kodithuwakku <126492783+Aweesha77@users.noreply.github.com>
2024-03-09 20:07:12 +0530

    second

--- weather_app/src/components/search/Search.js
+++ weather_app/src/components/search/Search.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { geoApiOptions, GEO_API_URL } from "../../Api";
 
 
 const Search = ({ onSearchChange }) => {
@@ -8,8 +9,22 @@ const Search = ({ onSearchChange }) => {
 
 
     const loadOptions = (inputValue) => {
-        
-    };
+        return fetch(
+          `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+          geoApiOptions
+        )
+          .then((response) => response.json())
+          .then((response) => {
+            return {
+              options: response.data.map((city) => {
+                return {
+                  value: `${city.latitude} ${city.longitude}`,
+                  label: `${city.name}, ${city.countryCode}`,
+                };
+              }),
+            };
+          });
+      };
 
     const handleOnChange = (searchData) => {
         setSearch(searchData);
